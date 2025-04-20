fbc959a1e9a0c8addd215a43368323df498034fe
Renato Fagalde <renato@likwi.com.br>
2025-04-20 20:54:07 -0300

    :recycle: refactor: api group

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bootstrap/internal/adapter/input/api/middleware"
 	"bootstrap/internal/adapter/input/api/routes"
 	"bootstrap/internal/config/logger"
 	"github.com/gin-gonic/gin"
@@ -9,7 +10,8 @@ import (
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
-	routes.InitRoutes(router)
+	router.Use(middleware.HeadersMiddleware())
+	routes.InitRoutes(&router.RouterGroup)
 	if err := router.Run(":8080"); err != nil {
 		logger.Error("Error starting server on port 8080", err)
 	}
