e4954780673e74ec4e222b3e6895810a4db17bc7
Renato Fagalde <renato@likwi.com.br>
2025-04-21 12:28:19 -0300

    :white_check_mark: feat: gorm setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,15 +3,34 @@ package main
 import (
 	"bootstrap/internal/adapter/input/api/middleware"
 	"bootstrap/internal/adapter/input/api/routes"
+	postgres "bootstrap/internal/config/database/postgres/gorm"
 	"bootstrap/internal/config/logger"
+	tools "bootstrap/tool"
+	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
 func main() {
+
+	var tools tools.ToolLoadEnvironmet
+
+	sqlConfig, err := tools.Do()
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Erro FATAL %+v", err))
+	}
+
+	database, err := postgres.NewPostgresGORMConnection(context.Background(), sqlConfig.DBSource)
+	if err != nil {
+		log.Fatalf("Erro ao conectar no banco, error=%s", err.Error())
+	}
+
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 	router.Use(middleware.HeadersMiddleware())
-	routes.InitRoutes(&router.RouterGroup)
+
+	routes.InitRoutes(&router.RouterGroup, database)
 	if err := router.Run(":8080"); err != nil {
 		logger.Error("Error starting server on port 8080", err)
 	}
